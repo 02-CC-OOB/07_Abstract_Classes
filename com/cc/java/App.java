6bc8e5f41ea4948dc4d86cdc381c0883706865e3
Sebas <cell333666@gmail.com>
2022-11-29 12:56:57 +0100

    abstrakte klassen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,12 +3,39 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+    
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Triangle triangle = new Triangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+
+        
+        output("------  Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area());
+
+        output("------  Differenzen ------");
+        output("Diff. r-t: " + areaDiff(rectangle, triangle));
+
+
+        output("Diff. r-c: " + areaDiff(rectangle, circle));
+
+
     }
+
+
+    private static double areaDiff(Shape  s1, Shape s2) {
+            return s1.area() - s2.area();
+    }
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
+
+
+    
 
 
 
